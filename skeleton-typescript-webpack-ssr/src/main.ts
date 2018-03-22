6f291ba3e16e9811624fa2c26109e7560bee8663
Romke van der Meulen <romke.vd.meulen@gmail.com>
2018-03-22 17:58:32 +0100

    doc(main): remove example plugin configuration
    Though these are a handy way of helping new developers learn to extend
    their Aurelia app, they don't adequately explain the additional steps
    needed to install a plugin. Remove the comments and leave it to the
    plugin doc to explain how to install them.

--- skeleton-typescript-webpack-ssr/src/main.ts
+++ skeleton-typescript-webpack-ssr/src/main.ts
@@ -10,13 +10,6 @@ export async function configure(aurelia: Aurelia) {
     .standardConfiguration()
     .developmentLogging();
 
-  // Uncomment the line below to enable animation.
-  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
-  // if the css animator is enabled, add swap-order="after" to all router-view elements
-
-  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
-
   await aurelia.start();
   await aurelia.setRoot(PLATFORM.moduleName('app'));
 }
